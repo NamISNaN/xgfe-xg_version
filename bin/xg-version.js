9161c6684792a8dadc3d3fe92634150cdfd9cfe8
陈磊 <chenlei65@meituan.com>
2020-11-19 00:56:42 +0800

    feat[TMS](TMS) 封板前代码提交

--- bin/xg-version.js
+++ bin/xg-version.js
@@ -130,7 +130,7 @@ function git(code){
     console.log('\x1B[36m%s\x1B[0m',code)
    shell.exec(code,{fatal:true},function(code, stdout, stderr) {
      console.error('\x1B[31m%s\x1B[0m',stderr)
-     if (stderr!=''){
+     if (code===1){
          checkError().then(res=>{
            res===0?resolve(stdout):process.exit(1)
          })

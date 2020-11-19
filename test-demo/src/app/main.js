4627483feab644f533baaf34dc0024da3eb7aa08
陈磊 <chenlei65@meituan.com>
2020-11-19 19:40:44 +0800

    feat[TMS](TMS) 修改版本号

--- test-demo/src/app/main.js
+++ test-demo/src/app/main.js
@@ -22,7 +22,7 @@ window.basePath = '';
 window.baseUrl = '/assets/';
 window.ver = '${__timestamp__}';
 // 系统版本号
-window.appVersion = 'v6.6.2.4';
+window.appVersion = 'v6.6.3.0';
 // 配置分组cookie
 if (GROUP_PLUS_ENV) {
     let exp = new Date();
